c935b6d403c2a49e9411fe66a26ab9d2da4ebb2b
minhaejo <robo888@naver.com>
2022-06-25 17:23:09 +0900

    first commit

---
+++ 1-type/1-1-basic.ts
@@ -0,0 +1,18 @@
+{
+    //number
+    const num:number = 10
+    //string
+    const string:string ="dd"
+    //boolean
+    const boal:boolean =true
+    //undifined
+    let age:number|undefined
+    age=undefined
+    age=1;
+
+    //null
+    let person2:string | null
+    person2="asd"
+    person2=null
+
+}
